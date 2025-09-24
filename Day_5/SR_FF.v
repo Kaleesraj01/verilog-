5cabd642143eb0575906057bb9e90006cb4be38c
Kaleesraj M <kalishraj01@gmail.com>
2025-09-24 15:49:22 +0530

    Create SR_FF.v

---
+++ Day_5/SR_FF.v
@@ -0,0 +1,14 @@
+module SR_ff (input  wire s,
+    input  wire r,
+    input  wire clk,
+    output reg  q);
+always @(posedge clk) begin
+case({s,r})
+2'b00: q <= q;
+2'b01: q <= 0;
+2'b10: q <= 1;
+2'b11: q <= 1'bx;
+default: q <= q;
+endcase
+end
+endmodule
