4bd5e902c2e1aa570ac4facae4d17a788a339df6
Kaleesraj M <kalishraj01@gmail.com>
2025-09-25 16:45:41 +0530

    Create 4bit_updowncounter.v

---
+++ Day_6/4bit_updowncounter.v
@@ -0,0 +1,10 @@
+module updowncounter(input clk , reset , updown , output reg [3:0] count );
+always @(posedge clk or posedge reset ) begin
+ if (reset)
+count <= 4'b0000;
+else if (updown )
+count <= count+1;
+ else
+ count <= count-1 ;
+end
+endmodule
