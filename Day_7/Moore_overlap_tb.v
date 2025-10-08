792291a825e8327611ead13a62f94089bb182e34
Kaleesraj M <kalishraj01@gmail.com>
2025-10-08 21:34:12 +0530

    Create Moore_overlap_tb.v

---
+++ Day_7/Moore_overlap_tb.v
@@ -0,0 +1,32 @@
+module tb_moore_10110;
+reg clk,rst,din;
+wire dout;
+moore10110 uut(.clk(clk),.rst(rst),.din(din),.dout(dout));
+
+initial clk=0;
+always #5 clk=~clk;
+
+initial begin
+rst=1; din=0; #10;
+rst=0;
+din=1; #10; 
+din=0; #10; 
+din=1; #10; 
+din=1; #10; 
+din=0; #10; 
+din=1; #10; 
+
+din=0; #10; 
+din=1; #10;
+din=1; #10; 
+din=0; #10; 
+din=0; #10;
+$finish;
+end
+
+initial begin
+$monitor("Time=%0t din=%b dout=%b",$time,din,dout);
+$dumpfile("raj.vcd");
+$dumpvars();
+end
+endmodule
