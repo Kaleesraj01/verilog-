b779fdd492c4f92ccc81ae6cc124fc33cc11e7f6
Kaleesraj M <kalishraj01@gmail.com>
2026-03-02 10:26:12 +0530

    Update pattern .v

--- patterns /pattern .v
+++ patterns /pattern .v
@@ -16,5 +16,24 @@ OUTPUT
 # KERNEL: @@@
 # KERNEL: @@@@
 # KERNEL: @@@@@
+
+
+
+module pattern ;
+  integer k , r ;
+
+  initial begin 
+    for ( k = 4 ; k >= 1 ; k= k-1) begin 
+      for ( r = 1 ; r<=k ; r = r+1) begin
+        $write ("&");
+      end 
+      $display (""); end 
+  end
+endmodule 
+  
+# KERNEL: &&&&
+# KERNEL: &&&
+# KERNEL: &&
+# KERNEL: &
   
       
