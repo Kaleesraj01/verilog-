91500eeb90a1c5b15b5ee2518c65d3983a948255
Kaleesraj M <kalishraj01@gmail.com>
2026-03-02 10:25:32 +0530

    Create pattern .v

---
+++ patterns /pattern .v
@@ -0,0 +1,20 @@
+module pattern ;
+  integer k , r ;
+
+  initial begin 
+    for ( k = 1 ; k <= 5 ; k= k+1) begin 
+      for ( r = 1 ; r<=k ; r = r+1) begin
+        $write ("@");
+      end 
+      $display (""); end 
+  end
+endmodule 
+
+OUTPUT 
+# KERNEL: @
+# KERNEL: @@
+# KERNEL: @@@
+# KERNEL: @@@@
+# KERNEL: @@@@@
+  
+      
