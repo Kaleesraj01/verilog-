880342a82a5266116bcec175df292daf3385e66b
Kaleesraj M <kalishraj01@gmail.com>
2026-03-24 11:14:03 +0530

    Update Updowncounter.v

--- Day_6/Updowncounter.v
+++ Day_6/Updowncounter.v
@@ -24,4 +24,45 @@
         end
     end
 endmodule
+
+
+TESTBENCH
+module tb ;
+   logic clk , rst , en ;
+  logic [3:0] cout ;
+  
+  updowncounter dut (.clk(clk), .rst(rst), .en(en) , .cout(cout));
+   
+  initial begin 
+     
+  forever #5 clk = ~ clk ;
+  
+    
+       $dumpfile("wave.vcd");  
+    $dumpvars(0, tb);
+  $monitor("Time=%0t clk=%0b rst=%0b en=%0b cout=%0d",
+          $time, clk, rst, en, cout);
+
+    // Initialize
+    clk = 0;
+    rst = 1;
+    en  = 0;
+
+    #10;
+    rst = 0;   
+
+    #10;
+    en = 1;    
+
+    #200;      
+
+    en = 0;    
+    #50;
+
+    en = 1;   
+    #100;
+
+    $finish;
+  end
+endmodule 
             
