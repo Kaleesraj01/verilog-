2fbadb33c8f8e1dd6bb3ade4255cbc70a293aa4a
Kaleesraj M <kalishraj01@gmail.com>
2026-03-25 23:59:37 +0530

    Create counter seq.v

---
+++ Day_6/counter seq.v
@@ -0,0 +1,21 @@
+module custom_counter (
+    input clk,
+    input rst,
+    output reg [2:0] state
+);
+
+always @(posedge clk) begin
+    if (rst)
+        state <= 3'b000;
+    else begin
+        case (state)
+            3'b000: state <= 3'b011;
+            3'b011: state <= 3'b101;
+            3'b101: state <= 3'b110;
+            3'b110: state <= 3'b000;
+            default: state <= 3'b000; // invalid states
+        endcase
+    end
+end
+
+endmodule
